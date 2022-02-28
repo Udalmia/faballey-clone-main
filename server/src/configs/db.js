d79d730823c46f9efdc78480e3eba703971d31a9
udalmia <utkarshdalmia3@gmail.com>
2022-02-28 19:52:10 +0530

    files

--- server/src/configs/db.js
+++ server/src/configs/db.js
@@ -2,5 +2,6 @@ const mongoose = require("mongoose");
 module.exports = () => {
   return mongoose.connect(
     "mongodb+srv://akhilg123:akhilg123@cluster0.75ole.mongodb.net/faballey"
+    //test
   );
 };
